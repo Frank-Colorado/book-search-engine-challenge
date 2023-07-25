0e6708b6c1d120cb70bd6be8bec70550793edb99
Frank-Colorado <chawnkiee@gmail.com>
2023-07-24 20:40:29 -0400

    updated handleSaveBook hook

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -67,12 +67,14 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
+      // use SAVE_BOOK mutation
+      const { data } = await saveBook({
+        variables: { input: bookToSave },
+      });
+      console.log(data);
       if (!response.ok) {
         throw new Error("something went wrong!");
       }
-
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
