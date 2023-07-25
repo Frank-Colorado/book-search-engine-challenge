f701bf9f43d9c7a4d614b63ea50760c688fc8a38
Frank-Colorado <chawnkiee@gmail.com>
2023-07-24 20:10:00 -0400

    created login mutation in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,4 +21,30 @@ const resolvers = {
       throw new AuthenticationError("Not logged in");
     },
   },
+    Mutation: {
+        // This is a mutation called login that returns an Auth type.
+        login: async (_parent, { email, password }) => {
+            // Find the user with the email that was passed in
+            const userData = await User.findOne({ email });
+            // If there is no user with that email
+            if (!userData) {
+                // Throw an AuthenticationError
+                throw new AuthenticationError("Incorrect credentials");
+            }
+            // If there is a user with that email
+            // Check the password using the instance method created in the User model
+            const correctPw = await userData.isCorrectPassword(password);
+            // If the password is incorrect
+            if (!correctPw) {
+                // Throw an AuthenticationError
+                throw new AuthenticationError("Incorrect credentials");
+            }
+            // If email and password are correct
+            // Sign token using the signToken function imported from auth
+            const token = signToken(userData);
+            // Return an Auth type that contains the token and user data
+            return { token, userData };
+        },
+    
 };
+
