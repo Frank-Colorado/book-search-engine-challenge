dc4eaa437470e4d555a75562edc907ed26630f8b
Frank-Colorado <chawnkiee@gmail.com>
2023-07-24 20:29:53 -0400

    created ADD_USER mutation in mutations.js

--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -1,9 +1,23 @@
 import { gql } from "@apollo/client";
 
-// This is mutation called LOGIN_USER that returns an Auth type.
+// This is a mutation called LOGIN_USER that returns an Auth type.
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+// This is a mutation called ADD_USER that returns an Auth type.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
