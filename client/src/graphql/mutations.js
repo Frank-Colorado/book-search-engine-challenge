d3a93d88c5c55c12ccd55fcbacf22050e7fab51b
Frank-Colorado <chawnkiee@gmail.com>
2023-07-26 18:06:20 -0400

    created mutations.js file in graphql folder

---
+++ client/src/graphql/mutations.js
@@ -0,0 +1,69 @@
+import { gql } from "@apollo/client";
+
+// This is a mutation called LOGIN_USER that returns an Auth type.
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+// This is a mutation called ADD_USER that returns an Auth type.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+// This is a mutation called SAVE_BOOK that returns a User type.
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+// This is a mutation called REMOVE_BOOK that returns a User type.
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
