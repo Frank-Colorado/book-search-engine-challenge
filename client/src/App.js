48a1fb88fede43299505f44490ffeb9b3a67ed7d
Frank-Colorado <chawnkiee@gmail.com>
2023-07-26 18:02:34 -0400

    updated app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+  defaultDataIdFromObject,
+} from "@apollo/client";
 
-function App() {
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// All component imports
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+
+// Setting up Apollo Client
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    User: {
+      fields: {
+        savedBooks: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+
+  dataIdFromObject: (object) => {
+    switch (object.__typename) {
+      case "User":
+        return object._id;
+      default:
+        return defaultDataIdFromObject(object);
+    }
+  },
+});
+
+const authLink = setContext((_, { headers }) => {
+  // Get token from local storage
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Linking to the server
+  link: authLink.concat(httpLink),
+  cache: cache,
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
